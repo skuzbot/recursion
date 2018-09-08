4ba6e452a8c61c7a0af256f89ca2e8da325c0509
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-08 17:53:28 -0500

    Fixed Arrays, Added Test Array

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -15,28 +15,57 @@
 var arr = [1, 2, 3, 4]; 		//  '[1,2,3,4]'
 var obj = {a: 1, b: 2, c: 3}; 	//	'{"a":1,"b":2,"c":3}'
 
-
+var stringifiableObjects = [
+  9,
+  null,
+  true,
+  false,
+  'Hello world',
+  [],
+  [8],
+  ['hi'],
+  [8, 'hi'],
+  [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+  [8, [[], 3, 4]],
+  [[[['foo']]]],
+  // {},
+  // {'a': 'apple'},
+  // {'foo': true, 'bar': false, 'baz': null},
+  // {'boolean, true': true, 'boolean, false': false, 'null': null },
+  // // basic nesting
+  // {'a': {'b': 'c'}},
+  // {'a': ['b', 'c']},
+  // [{'a': 'b'}, {'c': 'd'}],
+  // {'a': [], 'c': {}, 'b': true}
+];
 
 
 
 
 var stringifyJSON = function(obj) {
 
-        //****check for undefined outputs
+    //  check for undefined outputs
     if (typeof obj === 'function' || typeof obj === undefined) {
         return undefined;
     }
 
-	//****check for primitive types
-	if (typeof obj === 'number' || typeof obj === 'boolean' || typeof obj === null) {
+
+	// check for primitive types
+	if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
 		return String(obj);
 	}
 
-    //****check for string
+    //
+
+    //  check for string
     if (typeof obj === 'string') {
         return '"' + obj + '"';
     }
 
+    //  check for array
+    if (Array.isArray(obj)) {
+        return '[' + obj.map(stringifyJSON) + ']';
+    }
 
 
 
@@ -44,10 +73,10 @@ var stringifyJSON = function(obj) {
 
 
 //console.log(func(num));
-//console.log(JSON.stringify(num));
-var expected = JSON.stringify(str);
+console.log('JSON' + JSON.stringify(stringifiableObjects));
+var expected = JSON.stringify(stringifiableObjects);
 
-//console.log(stringifyJSON(num));
-var actual = stringifyJSON(str);
+console.log('Mine' + stringifyJSON(stringifiableObjects));
+var actual = stringifyJSON(stringifiableObjects);
 
 console.log(expected === actual);

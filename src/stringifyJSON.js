a3e63e57246afa0f6cc57e4351ccbc1d6451279d
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-08 17:37:26 -0500

    Check for undefined and primitives
    function, undefined, numbers, strings, booleans, null

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,51 @@
 
 // but you don't so you're going to write it from scratch:
 
+//****done****//
+// var func = function(x) {return x;}; // undefined
+// 'undefined'
+// var num = 298;      // '298'
+// var str = 'goose';  // '"goose"'
+// var boo = true; // 'true' or 'false'
+// 'null'
+
+
+var arr = [1, 2, 3, 4]; 		//  '[1,2,3,4]'
+var obj = {a: 1, b: 2, c: 3}; 	//	'{"a":1,"b":2,"c":3}'
+
+
+
+
+
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+        //****check for undefined outputs
+    if (typeof obj === 'function' || typeof obj === undefined) {
+        return undefined;
+    }
+
+	//****check for primitive types
+	if (typeof obj === 'number' || typeof obj === 'boolean' || typeof obj === null) {
+		return String(obj);
+	}
+
+    //****check for string
+    if (typeof obj === 'string') {
+        return '"' + obj + '"';
+    }
+
+
+
+
 };
+
+
+//console.log(func(num));
+//console.log(JSON.stringify(num));
+var expected = JSON.stringify(str);
+
+//console.log(stringifyJSON(num));
+var actual = stringifyJSON(str);
+
+console.log(expected === actual);
